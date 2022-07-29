84a742e624cf022688590dd73d87a7577f1f82d0
Simón Calderón <simoncalderonl2010@gmail.com>
2022-07-29 02:11:22 -0500

    Configuracion Inicial

---
+++ contratos.py
@@ -0,0 +1,17 @@
+"""
+Universidad de los Andes
+
+Proyecto Nivel 4
+
+Juan Jose Rivera Agudelo
+
+Código Estudiante:
+
+Correo Uniandes:
+
+"""
+
+import pandas as pd
+import matplotlib.pyplot as plt
+import numpy as np
+
